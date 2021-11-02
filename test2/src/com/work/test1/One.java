a2236c8372cd7787804edba343ca95c797c6a77d
Deepan234 <deepangtm2000@gmail.com>
2021-11-02 13:08:01 +0530

    First Commit

---
+++ test2/src/com/work/test1/One.java
@@ -0,0 +1,10 @@
+package com.work.test1;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Executing one.java File");
+	}
+
+}
---
+++ test2/src/com/work/test1/Two.java
@@ -0,0 +1,10 @@
+package com.work.test1;
+
+public class Two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Executing Two.java file");
+	}
+
+}
